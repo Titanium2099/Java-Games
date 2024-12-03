b6be9c7612ae05b8da038de9e54b476d2cb00eb4
Titanium2099 <yasgh@protonmail.com>
2024-12-02 17:14:21 -0800

    added font and cursor change

--- app/src/main/java/org/example/MainMenuButton.java
+++ app/src/main/java/org/example/MainMenuButton.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 
 public class MainMenuButton extends Button {
 
@@ -13,8 +14,14 @@ public class MainMenuButton extends Button {
         // Set fixed width and height (JavaFX ignores -fx-width/-fx-height in CSS)
         this.setPrefWidth(300);
         this.setPrefHeight(60);
-
-
+        
+        //load custom font
+        Font quicksand = Font.loadFont(getClass().getResource("/fonts/quicksand.ttf").toExternalForm(), 18);
+        if (quicksand != null) {
+            this.setFont(quicksand); // Apply font to the button
+        } else {
+            System.out.println("Font could not be loaded!");
+        }
         //weird fix to border radius glitch
         this.heightProperty().addListener((obs, oldHeight, newHeight) -> {
             double radius = newHeight.doubleValue() / 2;
@@ -29,7 +36,13 @@ public class MainMenuButton extends Button {
             if (!this.getStyleClass().contains("button-hovered")) {
                 this.getStyleClass().add("button-hovered");
             }
+            //change cursor
+            this.getScene().setCursor(javafx.scene.Cursor.HAND);
         });
-        this.setOnMouseExited(e -> this.getStyleClass().remove("button-hovered"));
+        this.setOnMouseExited(e -> {
+            this.getStyleClass().remove("button-hovered");
+            //change cursor
+            this.getScene().setCursor(javafx.scene.Cursor.DEFAULT);
+        });
     }
 }
